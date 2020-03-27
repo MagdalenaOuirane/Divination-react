df2ef09c13a212b006a7408d9f6a2e98fdd269bc
MagdalenaOuirane <magdalenaouirane@gmail.com>
2020-03-27 22:58:32 +0100

    add style and div with input

--- src/NameMap.js
+++ src/NameMap.js
@@ -14,21 +14,33 @@ const NameMap = (props) => (
        Update
         </button>
 
+        <button style ={{marginLeft:'5px'}} onClick={props.onDelete} type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Delete</button> 
+
 
 <div className="modal fade" id="exampleModalCenter" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div className="modal-dialog modal-dialog-centered" role="document">
     <div className="modal-content">
       <div className="modal-header">
-        <h5 className="modal-title" id="exampleModalLongTitle">Update name</h5>
+        <h5 className="modal-title" id="exampleModalLongTitle">UPDATE NAME</h5>
         <button type="button" className="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div className="modal-body">
         <h1> Are you sure that you want to update name?</h1>
+        <div  style={{marginTop: '50px'}}className="input-group mb-3">
+  <div className="input-group-prepend">
+    <span className="input-group-text" id="inputGroup-sizing-default">Name</span>
+  </div>
+  <input 
+   type="text" 
+   className="form-control" 
+   aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"
+   value={props.nameValue}
+   />
+</div>
         
-        
-<p>{props.nameValue}</p>
 
   </div>
       <div className="modal-footer">
@@ -40,19 +52,7 @@ const NameMap = (props) => (
 </div>
 </div>
 
-
-
-  <br /> 
-
-       
-
-<br/>
- <button onClick={props.onDelete} type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Delete</button>    
-
- 
-
-    </div>
+ </div>
 )
 
 export default NameMap;
